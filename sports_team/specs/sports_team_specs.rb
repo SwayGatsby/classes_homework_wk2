72d42179ebb3adec97b7ecff48a409a4fb60d4c1
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-02 19:03:03 +0100

    all sports_team getter tests passed

--- sports_team/specs/sports_team_specs.rb
+++ sports_team/specs/sports_team_specs.rb
@@ -8,3 +8,17 @@ class TestSportsTeam < MiniTest::Test
   def setup
     @sports_team = SportsTeam.new("Detroit Pistons", ["Grant Hill", "Isaiah Thomas", "Ben Wallace"], "Larry Brown")
   end
+
+  def test_team_name
+    assert_equal("Detroit Pistons", @sports_team.team_name)
+  end
+
+  def test_players
+    assert_equal(["Grant Hill", "Isaiah Thomas", "Ben Wallace"], @sports_team.players)
+  end
+
+  def test_coach
+    assert_equal("Larry Brown", @sports_team.coach)
+  end
+
+end
--- sports_team/sports_team.rb
+++ sports_team/sports_team.rb
@@ -1,11 +1,23 @@
 #Make a class to represent a Team that has the properties Team name (String), Players (Array of strings) and a Coach (String).
 
-Class SportsTeam
+class SportsTeam
 
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+  end
+
+  def team_name
+    return @team_name
+  end
+
+  def players
+    return @players
+  end
+
+  def coach
+    return @coach
   end
 
 end
